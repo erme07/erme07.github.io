f9d979004cc0280b74fa13e25dd0e07a24ea64ef
Erik <46094137+erme07@users.noreply.github.com>
2022-05-27 23:17:59 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-//javascript for navigation bar effect on scroll
+//ocultar y mostra la barra de menu haciendo scroll
+
 const body = document.body;
 const scrollArriba = "scroll-arriba";
 const scrollAbajo = "scroll-abajo";
@@ -14,7 +15,7 @@ if(windowWidth<768){
     return;
     }
    
-    if (posicionActual > posicionAnterior && !body.classList.contains(scrollAbajo)) {
+    if (posicionActual > posicionAnterior && !body.classList.contains(scrollAbajo) && posicionActual>48) {
       // bajar
       body.classList.remove(scrollArriba);
       body.classList.add(scrollAbajo);
@@ -48,7 +49,8 @@ if(windowWidth<768){
 
 
 
-//javascript for responsive navigation sidebar menu
+//mostrar y ocultar menu en celulares
+
 var barraMenu = document.querySelector('.barra-navegacion');
 var menuBtn = document.querySelector('.menu-btn');
 var closeBtn = document.querySelector('.close-btn');
@@ -60,3 +62,64 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   barraMenu.classList.remove('active');
 });
+
+
+//verificar en que pagina de la web estoy
+
+function filename(){
+  var rutaAbsoluta = self.location.href;   
+  var posicionUltimaBarra = rutaAbsoluta.lastIndexOf("/");
+  var rutaRelativa = rutaAbsoluta.substring( posicionUltimaBarra + "/".length , rutaAbsoluta.length );
+  return rutaRelativa;
+}
+	var nombre_archivo = filename();
+
+
+	if(nombre_archivo === 'contacto.html'){
+    barraMenu.classList.add('select-contacto');
+  }
+  if(nombre_archivo === 'index.html'){
+    barraMenu.classList.add('select-inicio');
+  }
+  if(nombre_archivo === 'noticias.html'){
+    barraMenu.classList.add('select-noticias');
+  }
+  
+
+
+////modo oscuro
+const preferedColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+const slider = document.getElementById('slider');
+
+const setTheme = (theme) => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+}
+
+slider.addEventListener('click', ()  => {
+    let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+    setTheme(switchToTheme);
+});
+
+setTheme(localStorage.getItem('theme') || preferedColorScheme);
+
+
+//circulo review
+
+let progressCircle = document.querySelector(".progress");
+    let radius = progressCircle.r.baseVal.value;
+    //circumference of a circle = 2πr;
+    let circumference = radius * 2 * Math.PI;
+    progressCircle.style.strokeDasharray = circumference;
+
+    //0 to 100
+  let porcentaje = 65;
+
+    setProgress(porcentaje);
+
+    function setProgress(percent) {
+        progressCircle.style.strokeDashoffset = circumference - (percent / 100) * circumference;
+    }
+
+    let textoPorcentaje = document.querySelector(".reseña-porcentaje text")
+     textoPorcentaje.innerHTML = (porcentaje/10);
