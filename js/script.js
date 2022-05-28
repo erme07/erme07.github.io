e1385a3d0121fc42a5d2211446c619ab7323a41d
Erik <46094137+erme07@users.noreply.github.com>
2022-05-28 18:13:20 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -79,7 +79,7 @@ var rutAbsoluta = self.location.href;
 	if(nombre_archivo === 'contacto.html'){
     barraMenu.classList.add('select-contacto');
   }
-  if(nombre_archivo === 'index.html' || rutAbsoluta === 'https://erme07.github.io'){
+  if(nombre_archivo === 'index.html' || rutAbsoluta === 'https://erme07.github.io/'){
     barraMenu.classList.add('select-inicio');
   }
   if(nombre_archivo === 'noticias.html'){
